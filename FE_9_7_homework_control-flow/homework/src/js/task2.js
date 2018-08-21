06ba74cfdb03e6de982d200e270a14a74a24f8b9
Marta Klaptiuk <tusyvfhnf@gmail.com>
2018-08-21 22:02:17 +0300

    1

--- FE_9_7_homework_control-flow/homework/src/js/task2.js
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -1,99 +1,35 @@
-let wannaPlay = confirm('Do you want to play a game?');
-let userNum;
-let prize = 0;
-let gameNum = 0;
-let min = 0;
-let max = 5;
-const maxNum = 6;
+const MAX_RANGE = 5, POSSIBLE_PRIZE = 10;
 
-let numbers = { one: 1, two: 2, three: 3, ten: 10, five: 5 };
+confirm('Do you want to play a game?') ? startGame() : alert('You did not become a millionaire, but can.');
 
-let one = 10;
-let two = 5;
-let three = 2;
+function startGame(maxRange = MAX_RANGE, attemptsLeft = 3, totalPrize = 0, possiblePrize = POSSIBLE_PRIZE) {
+  const randomNumber = generateRandomNumber(maxRange);
 
-if (wannaPlay === true) {
-  game();
-} else {
-  alert('You did not become a millionaire, but can.');
-}
+  for (let curPrize = possiblePrize; attemptsLeft > 0; curPrize = Math.floor(curPrize / 2), attemptsLeft--) {
+    const userNumber = parseInt(prompt('Enter a number from 0 to ' + maxRange + '\nAttempts left: ' + attemptsLeft +
+      '\nTotal prize: ' + totalPrize + '$\nPossible prize on current attempt: ' + curPrize + '$'));
 
-function game() {
-  gameNum++;
-  if (gameNum >= 2) {
-    one *= 3;
-    two *= 3;
-    three *= 3;
-  }
-  let tries = 0;
-  let number = Math.floor(Math.random() * maxNum);
-  console.log(number);
+    if (!isNaN(userNumber) && userNumber === randomNumber) {
+      totalPrize += curPrize;
 
-  while (userNum !== number && tries < 3) {
-    tries++;
-
-    let attemptAmount = 0;
-    if (tries === numbers.one) {
-      attemptAmount = one;
-    } else if (tries === numbers.two) {
-      attemptAmount = two;
-    } else if (tries === numbers.three) {
-      attemptAmount = three;
-    }
-
-    let result1 = 'Enter a number from ' + min * gameNum + ' to ' + max * gameNum + '\n';
-    let result2 = 'Attempts left: ' + (4 - tries) + '\n';
-    let result3 = 'Total prize: ' + prize + '$' + '\n';
-    let result4 = 'Possible prize on current attempt: ' + attemptAmount + '$';
-
-
-    userNum = +prompt(result1 + result2 + result3 + result4, '');
-  }
-
-  if (tries === numbers.one && userNum === number) {
-    prize += one;
-  } else if (tries === numbers.two && userNum === number) {
-    prize += two;
-  } else if (tries === numbers.three && userNum === number) {
-    prize += three;
-  }
-
-  let playAgain;
-  let playContinue;
-  if (userNum !== number) {
-    alert('Thank you for a game. Your prize is: ' + prize + '$');
-    playAgain = confirm('Are you wants to play again?', '');
-
-    if (playAgain === true) {
-      gameNum = 0;
-      prize = 0;
-      min = 0;
-      max = numbers.five;
-      one = numbers.ten;
-      two = numbers.five;
-      three = 2;
-      game();
-    }
-  } else {
-    playContinue = confirm('Congratulation! Your prize is: ' + prize + '$' + '. Do you want to continue?');
-
-    if (playContinue === true) {
-      game();
-    } else {
-      alert('Thank you for a game. Your prize is: ' + prize + '$');
-      playAgain = confirm('Are you wants to play again?', '');
-
-      if (playAgain === true) {
-        gameNum = 0;
-        prize = 0;
-        min = 0;
-        max = numbers.five;
-        one = numbers.ten;
-        two = numbers.five;
-        three = 2;
-        game();
+      if (confirm('Congratulation! Your prize is: ' + totalPrize + '$ \nDo you want to continue?')) {
+        startGame(maxRange * 2, 3, totalPrize, possiblePrize * 3);
       }
+
+      break;
+    } else if (isNaN(userNumber) || !--attemptsLeft) {
+      // user canceled prompt input OR last attempt, and user failed to guess a random number
+      alert('Thank you for a game. Your prize is: ' + totalPrize + '$');
+
+      if (confirm('Do you want to play again?')) {
+        startGame();
+      }
+
+      break;
     }
   }
 }
 
+function generateRandomNumber(maxRange) {
+  return Math.floor(Math.random() * ++maxRange);
+}
